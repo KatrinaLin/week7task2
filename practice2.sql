d5381aee24f0058e93f52c17c29fe494865d9ae6
KatrinaLin <33877748+KatrinaLin@users.noreply.github.com>
2018-05-04 19:00:26 +0800

    Update practice2.sql

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,9 @@
 # 取得每个company中最高薪水的人员名字
 # 输出结果包含公司名称和人员名称：companyName name
+
+SELECT companyName, name
+FROM Employee INNER JOIN Company on Employee.companyId = Company.id
+WHERE (companyName, salary) IN
+(SELECT companyName, MAX(salary) 
+ FROM Employee INNER JOIN Company on Employee.companyId = Company.id 
+ Group By Company.companyName);
