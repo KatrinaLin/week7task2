0736750cc9cb35dbe0af36ca5851c0fc3d7a3382
KatrinaLin <33877748+KatrinaLin@users.noreply.github.com>
2018-05-04 18:56:52 +0800

    Update practice3.sql

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,7 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
-
+SELECT companyName, AVG(salary) as avgSalary 
+FROM Employee INNER JOIN Company on Employee.companyId = Company.id 
+Group By Company.companyName 
+Order by avgSalary Desc limit 1;
